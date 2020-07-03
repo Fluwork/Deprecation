2bfc9ced4d0ff5b755956c2b24665bf4bf0ae46c
YaFou <33806646+YaFou@users.noreply.github.com>
2020-07-03 12:11:11 +0200

    added: "trigger_deprecation" function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,8 @@
 # Changelog
 This changelog follows [the "Changelog specification" 1.0.0 *beta 2*](https://github.com/YaFou/Changelog-specification).
+
+## 1.0.0
+> 2020-07-03
+
+### Added
+- `trigger_deprecation` function.
---
+++ src/function.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Fluwork\Deprecation;
+
+if (!function_exists(__NAMESPACE__ . '\trigger_deprecation')) {
+    /**
+     * Prints a deprecation message.
+     *
+     * @param string $package
+     * @param string $depreciationVersion The version in which the deprecation is introduced
+     * @param string $depreciation The feature name which is deprecated
+     * @param string $message The deprecation message
+     * @param string $changeVersion The version where the deprecation change will be applied
+     * @param mixed[] ...$args The arguments of the message (with the "sprintf" function)
+     * @return void
+     *
+     * @author Yannis Foucher
+     */
+    function trigger_deprecation(
+        string $package,
+        string $depreciationVersion,
+        string $depreciation,
+        string $message,
+        string $changeVersion,
+        ...$args
+    ): void {
+        trigger_error(sprintf(
+            'Since "%s" %s, %s is deprecated: %s in version %s',
+            $package,
+            $depreciationVersion,
+            $depreciation,
+            vsprintf($message, $args),
+            $changeVersion
+        ), E_USER_DEPRECATED);
+    }
+}
